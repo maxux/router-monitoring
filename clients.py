43266b2355c88d7ffed9c86be5ab25091597fa05
Maxime Daniel <root@maxux.net>
2016-06-17 12:55:10 +0200

    adding dhcp leases on monitoring client

--- clients.py
+++ clients.py
@@ -14,9 +14,42 @@ class Colors():
 		self.white  = '\033[1;37m'
 		self.clear  = '\033[0m'
 
+class Monitoring():
+	def __init__(self):
+		self.colors  = Colors()
+		self.rows    = 0
+		self.columns = 0
+		self.index   = 0
+	
+	def initialize(self):
+		sys.stdout.write("\033[2J\033[H")
+		sys.stdout.write("\033[?25l")
+		self.rows, self.columns = os.popen('stty size', 'r').read().split()
+	
+	def new(self):
+		# new run
+		self.index = 0
+		
+		# clean screen
+		sys.stdout.write("\033[H")
+	
+	def separe(self):
+		sys.stdout.write("\033[K\n")
+		self.index += 1
+	
+	def clean(self):
+		# clean rest of the screen
+		while self.index < int(self.rows) - 1:
+			sys.stdout.write("\033[K\n")
+			index += 1
+		
+	def reset(self):
+		sys.stdout.write("\033[?25h\n")
+	
 class WirelessMonitor():
-	def __init__(self, interfaces=["wlan0"]):
+	def __init__(self, console, interfaces=["wlan0"]):
 		self.interfaces = interfaces
+		self.console = console
 		self.clients = {}
 		self.colors = Colors()
 	
@@ -157,20 +190,11 @@ class WirelessMonitor():
 	"""
 	Displayer
 	"""
-	def initialize(self):
-		sys.stdout.write("\033[2J\033[H")
-		sys.stdout.write("\033[?25l")
-		self.rows, self.columns = os.popen('stty size', 'r').read().split()
-	
-	def reset(self):
-		sys.stdout.write("\033[?25h\n")
-
 	def refresh(self):
-		sys.stdout.write("\033[H")
-		print(" Station          | IP Address      | RX Data   | TX Data   | Signal")
+		print(" Wireless MAC     | IP Address      | RX Data   | TX Data   | Signal")
 		print("------------------+-----------------+-----------+-----------+----------")
 		
-		index = 2
+		self.console.index += 2
 		for client in self.clients:
 			client = self.clients[client]
 			
@@ -188,34 +212,161 @@ class WirelessMonitor():
 			
 			# end of line
 			sys.stdout.write("\033K\n")
-			index += 1
-			
-			if index == int(self.rows) - 1:
-				print("[+ %d more]" % len(self.clients) - index + 2)
-				break
-		
-		# clearing old lines
-		while index < int(self.rows) - 1:
-			sys.stdout.write("\033[K\n")
-			index += 1
+			self.console.index += 1
 
-wlz = WirelessMonitor(["wlan0", "wlan1"])
-wlz.initialize()
+class DHCPMonitor():
+	def __init__(self, console, leaseFiles=["/var/lib/dhcp/dhcpd.leases"]):
+		self.leaseFiles = leaseFiles
+		self.console = console
+		self.clients = {}
+		self.colors = Colors()
+	
+	"""
+	Source
+	"""
+	def readSource(self, leaseFile):
+		leases = {}
+		
+		with open(leaseFile, 'r') as content:
+			full = content.read()
+		
+		for lease in full.split("\nlease "):
+			inner = lease.split('{\n')
+			
+			if len(inner) < 2:
+				continue
+
+			host = lease.partition(' ')[0]
+			leases[host] = inner[1]
+		
+		for host in leases:
+			leases[host] = leases[host].split(";\n")
+			leases[host] = list(map(str.strip, leases[host]))
+			leases[host].pop()
+		
+		return leases
+	
+	def readArp(self):
+		output = []
+
+		proc = subprocess.Popen(['ip', 'n'], stdout=subprocess.PIPE)
+		for input in proc.stdout:
+			output.append(input.decode('utf-8').rstrip())
+		
+		return output
+	
+	def setActive(self):
+		arp = self.readArp()
+		table = {}
+		
+		for line in arp:
+			words = line.split(" ")
+			
+			if len(words) != 6:
+				continue
+			
+			table[words[4]] = words[0]
+
+		for client in self.clients:
+			self.clients[client]['state'] = "inactive"
+
+			if self.clients[client]['hardware'] in table:
+				self.clients[client]['state'] = "active"
+		
+	def _update(self, interface):
+		source = self.readSource(interface)
+		current = {}
+		
+		for client in source:
+			self.clients[client] = {}
+			
+			for line in source[client]:
+				temp = line.split(' ')
+				
+				if temp[0] == 'ends':
+					self.clients[client]['expire'] = '%s %s' % (temp[2], temp[3])
+				
+				if temp[0] == 'hardware':
+					self.clients[client]['hardware'] = temp[2]
+				
+				if temp[0] == 'client-hostname':
+					self.clients[client]['hostname'] = temp[1].strip('"')
+	
+		# grabbing ip address from arp cache
+		if len(self.clients) > 0:
+			self.setActive()
+	
+	def update(self):
+		self.clients = {}
+		
+		for leaseFile in self.leaseFiles:
+			self._update(leaseFile)
+
+	"""
+	Formatter
+	"""
+	def _colorizeState(self, client):
+		c = self.colors
+		color = c.green if client['state'] == "active" else c.red		
+
+		return color + client["state"] + c.clear
+	
+	"""
+	Displayer
+	"""	
+	def refresh(self):
+		print(" Client MAC       | IP Address      | Status    | Hostname")
+		print("------------------+-----------------+-----------+----------------------")
+		
+		self.console.index += 2
+		for client in self.clients:
+			host = client
+			client = self.clients[client]
+			
+			# sys.stdout.write(self._colorizeStation(client))
+			sys.stdout.write(client['hardware'])
+			sys.stdout.write(" | ")
+			
+			# sys.stdout.write(self._colorizeAddress(client))
+			sys.stdout.write("%-15s" % host)
+			sys.stdout.write(" | ")
+			
+			sys.stdout.write("%-20s | " % self._colorizeState(client))
+			
+			# sys.stdout.write(self._colorizeSignal(client))
+			if 'hostname' in client:
+				sys.stdout.write(client['hostname'])
+			
+			# end of line
+			sys.stdout.write("\033K\n")
+			self.console.index += 1
+
+monitor = Monitoring()
+monitor.initialize()
+
+dhcp = DHCPMonitor(monitor)
+wlz = WirelessMonitor(monitor, ["wlan0", "wlan1"])
 
 runid = 0
 
 while True:
 	if (runid % 20) == 0:
-		wlz.initialize()
+		monitor.initialize()
 
 	try:
+		monitor.new()
+
 		wlz.update()
+		dhcp.update()
+		
 		wlz.refresh()
+		monitor.separe()
+		dhcp.refresh()
 	
 		time.sleep(1)
 
 	except KeyboardInterrupt:
-		wlz.reset()
+		monitor.reset()
 		sys.exit(0)
 	
 	runid += 1
